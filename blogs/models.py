c304e367ef9f10b889be5f14db5c99764947f046
yangting <ythhh1@163.com>
2020-07-21 13:17:41 +0800

    sidebar 侧边栏 封装

--- blogs/models.py
+++ blogs/models.py
@@ -77,6 +77,9 @@ class Post(models.Model):
     owner = models.ForeignKey(User, verbose_name='作者', on_delete=models.PROTECT, db_constraint=False)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
 
+    pv = models.PositiveIntegerField(default=1)
+    uv = models.PositiveIntegerField(default=1)
+
     def __str__(self):
         return self.title
 
@@ -111,3 +114,7 @@ class Post(models.Model):
     @classmethod
     def latest_posts(cls):
         queryset = cls.objects.filter(status=cls.STATUS_NORMAL)
+
+    @classmethod
+    def hot_posts(cls):
+        return cls.objects.filter(status=cls.STATUS_NORMAL).order_by('-pv')
