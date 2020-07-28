75f0a63df069884767df3ac55e5b9b2566184391
yangting <ythhh1@163.com>
2020-07-28 13:34:35 +0800

    static-css

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -131,4 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = '/tmp/static'
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'themes', THEME, 'static')
+]
