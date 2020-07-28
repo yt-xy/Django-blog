bff5726219ccc231212f3d2fdce271b301190493
yangting <ythhh1@163.com>
2020-07-28 11:05:17 +0800

    Bootstrap-html

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -55,10 +55,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'blog.urls'
 
+THEME = 'bootstrap'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'themes', THEME, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
